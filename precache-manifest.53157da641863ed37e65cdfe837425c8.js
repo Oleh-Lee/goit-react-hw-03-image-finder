d7153781016e23a771cfbc6b08b22277a2c5dac6
Oleh <rrrgo3@gmail.com>
2020-03-17 20:50:35 +0200

    Updates

---
+++ precache-manifest.53157da641863ed37e65cdfe837425c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f27302b44ea5908272c97071098b80",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "55a29793d55579772360",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.c56f50c3.chunk.css"
+  },
+  {
+    "revision": "ce07a7c1efdd059a92d7",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c2183ee8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c2183ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a29793d55579772360",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.49cda09a.chunk.js"
+  },
+  {
+    "revision": "4992c48e99877600891f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ad90512f.js"
+  }
+]);
